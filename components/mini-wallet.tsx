f853bcb77772a119ea1ced93e69fe3fcd2e7d9b2
keplerpayadmin <keplerpay001@gmail.com>
2025-07-21 22:13:22 +0100

    Update mini-wallet.tsx

--- components/mini-wallet.tsx
+++ components/mini-wallet.tsx
@@ -31,38 +31,6 @@ import { ethers } from "ethers"
 // Definindo TOKENS para corresponder ao serviço de swap
 const TOKENS = [
   {
-    address: "0x2cFc85d8E48F8EAB294be644d9E25C3030863003",
-    symbol: "WLD",
-    name: "Worldcoin",
-    decimals: 18,
-    logo: "/images/worldcoin.jpeg",
-    color: "#000000",
-  },
-  {
-    address: "0x834a73c0a83F3BCe349A116FFB2A4c2d1C651E45",
-    symbol: "TPF",
-    name: "TPulseFi",
-    decimals: 18,
-    logo: "/images/logo-tpf.png",
-    color: "#00D4FF",
-  },
-  {
-    address: "0xEdE54d9c024ee80C85ec0a75eD2d8774c7Fbac9B", // Updated WDD address
-    symbol: "WDD",
-    name: "Drachma", // Updated name
-    decimals: 18,
-    logo: "/images/drachma-token.png", // Updated logo path
-    color: "#FFD700",
-  },
-  {
-    address: "0x79A02482A880bCE3F13e09Da970dC34db4CD24d1",
-    symbol: "USDC",
-    name: "USD Coin",
-    decimals: 6,
-    logo: "/images/usdc.png",
-    color: "#2775CA",
-  },
-  {
     address: "0x5fa570E9c8514cdFaD81DB6ce0A327D55251fBD4",
     symbol: "KPP", // Assuming KPP as symbol for KeplerPay
     name: "KeplerPay",
