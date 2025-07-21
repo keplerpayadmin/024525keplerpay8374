26ce01dab3c42a9033151cf2fc2d855a65d972fe
keplerpayadmin <keplerpay001@gmail.com>
2025-07-21 21:09:03 +0100

    Delete services directory

--- services/index.ts
+++
@@ -1,8 +0,0 @@
-// Exportar todos os serviços para facilitar importações
-export { holdstationService } from "./holdstation-service"
-export { holdstationHistoryService } from "./holdstation-history-service"
-export { walletService } from "./wallet-service"
-export { balanceSyncService } from "./balance-sync-service"
-
-// Exportar tipos
-export type { TokenBalance, SwapQuote, Transaction, TokenInfo } from "./types"
