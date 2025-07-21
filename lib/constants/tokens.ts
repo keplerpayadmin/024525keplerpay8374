d7a8d3a02d2723d20f9c99ae1922c2969cbb8616
keplerpayadmin <keplerpay001@gmail.com>
2025-07-21 18:42:25 +0100

    Add files via upload

---
+++ lib/constants/tokens.ts
@@ -0,0 +1,32 @@
+export const WORLDCHAIN_TOKENS = {
+  TPF: {
+    symbol: "TPF",
+    name: "TPulseFi",
+    address: "0x834a73c0a83F3BCe349A116FFB2A4c2d1C651E45",
+    decimals: 18,
+    image: "/images/logo-tpf.png",
+  },
+  WLD: {
+    symbol: "WLD",
+    name: "Worldcoin",
+    address: "0x2cFc85d8E48F8EAB294be644d9E25C3030863003",
+    decimals: 18,
+    image: "/images/worldcoin.jpeg",
+  },
+  WDD: {
+    symbol: "WDD",
+    name: "Drachma",
+    address: "0xEdE54d9c024ee80C85ec0a75eD2d8774c7Fbac9B",
+    decimals: 18,
+    image: "/images/drachma-token.png",
+  },
+  TPT: {
+    symbol: "TPT",
+    name: "TradePulse Token",
+    address: "0x868D08798F91ba9D6AC126148fdE8bBdfb6354D5",
+    decimals: 18,
+    image: "/images/logo-tpf.png",
+  },
+} as const
+
+export const PERMIT2_ADDRESS = "0x000000000022D473030F116dDEE9F6B43aC78BA3"
